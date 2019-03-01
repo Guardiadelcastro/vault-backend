aae4226bd4f6849589aa988ec25598d9860e02e6
Alejandro Castro <guardiadelcastro@gmail.com>
2019-03-01 16:31:17 +0000

    fixed error handling of registerUser

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,38 +1,28 @@
 import User from '../models/User';
 
 export async function registerUser(req, res) {
-  const { username, name, email, password } = req.body
-
-  if (!username || !name || !email || !password) {
-    res.status(403).json({ message: 'Unable to register user' })
-  }
-
   try{
-    const user = await User.findOne({ email: email })
+    const { username, name, email, password } = req.body;
+
+    if (!username || !name || !email || !password) {
+      res.status(403).json({ message: 'Unable to register user' });
+      return;
+    }
+
+    let user = await User.findOne({ email: email })
+
     if(user) {
       res.status(403).json({ message: 'Unable to register user' })
+      return;
     }
-  } catch(err) {
-    res.status(403).json({ message: 'Unable to register user' })
-    throw err
-  }
 
-  const user = new User({
-    username,
-    name,
-    email,
-    password
-  })
-
-  try{
+    user = new User({ username, name, email, password})
     await user.save()
-  } catch(err) {
-    res.status(403).json({ message: 'Unable to register user' })
-  }
+    res.status(200).json({ message: 'User Created',})
 
-  res.status(200).json({
-    message: 'User Created',
-  })
+  } catch(err) {
+    res.json({ message: 'Unable to register user' });
+  }
 
 }
 
